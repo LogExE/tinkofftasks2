2d2469736379c6887a097947688c500446d9724a
LogXx <awesome149712@gmail.com>
2024-03-12 22:38:56 +0400

    moar code

--- bot/src/main/java/edu/java/bot/clients/ScrapperClient.java
+++ bot/src/main/java/edu/java/bot/clients/ScrapperClient.java
@@ -1,8 +1,17 @@
 package edu.java.bot.clients;
 
+import edu.java.controller.dto.AddLinkRequest;
+import edu.java.controller.dto.LinkResponse;
+import edu.java.controller.dto.ListLinksResponse;
+import edu.java.controller.dto.RemoveLinkRequest;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import java.util.Set;
+
 
 @Component
 public class ScrapperClient {
@@ -34,20 +43,44 @@ public class ScrapperClient {
             .bodyToMono(String.class);
     }
 
-    Set<String> getAllTrackedLinks(long chatId) {
-        client
+    Set<URI> getAllTrackedLinks(long chatId) {
+        ListLinksResponse resp = client
             .get()
             .uri(LINKS_CONTROLLER_PATH)
             .header(TG_CHAT_HEADER, Long.toString(chatId))
             .retrieve()
-            .bodyToMono(String.class);
+            .bodyToMono(ListLinksResponse.class)
+            .block();
+        return resp.links().stream().map(LinkResponse::url).collect(Collectors.toSet());
     }
 
-    void addTrackedLink() {
-
+    void addTrackedLink(long chatId, String link) {
+        URI url;
+        try {
+            url = new URI(link);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        client
+            .post()
+            .uri(LINKS_CONTROLLER_PATH)
+            .header(TG_CHAT_HEADER, Long.toString(chatId))
+            .bodyValue(new AddLinkRequest(url))
+            .retrieve();
     }
 
-    void deleteTrackedLink() {
-
+    void deleteTrackedLink(long chatId, String link) {
+        URI url;
+        try {
+            url = new URI(link);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        client
+            .method(HttpMethod.DELETE)
+            .uri(LINKS_CONTROLLER_PATH)
+            .header(TG_CHAT_HEADER, Long.toString(chatId))
+            .bodyValue(new RemoveLinkRequest(url))
+            .retrieve();
     }
 }
